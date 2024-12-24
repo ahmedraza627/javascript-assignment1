58e33250b64015242c8c60cf2457c2b6985de3c5
Ahmed Raza <ahmedrazaafjn@gmail.com>
2024-12-24 18:54:13 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,86 @@
+// Assignment 1:
+
+// Chapter #1:
+
+// question 1
+// alert("Welcome to our Website")
+
+
+// question 2
+// alert("Please Enter a valid password")
+
+// question 3
+// alert("Welcome to JS Land... \n Happy Coding")
+
+// question4
+// alert("Welcome to JS Land")
+// alert("Happy Coding")
+// alert("Hello... I can run JS through my web browser's console")
+
+
+// Chapter #2:
+
+// question 1:
+
+// let username;
+
+// question 2:
+
+// let myName = "Ahmed Raza";
+
+// question 3:
+// let message;
+// message = "Hello World";
+// alert(message);
+
+// question 4:
+// let studentBio = prompt("Enter Your Name,age and Bio");
+// alert(studentBio);
+
+// question 5:
+
+// let word= alert("Pizza") 
+// let pizz= alert("pizz")
+// let Piz= alert("Piz") 
+// let Pi= alert("Pi")
+// let p= alert("P")
+
+// question 6:
+
+// let email = "ahmedrazaafjn@gmail.com"
+// alert(`My email address is : ${email}`);
+
+// question 7:
+
+// let book= "A smarter way to learn JavaScript"
+// alert(`I am learning from the book ${book}`);
+
+// question 8:
+
+// document.write("Yah! I can write HTML content through JavaScript")
+
+// question 9
+
+// let symbol=“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+// alert(symbol);
+
+// chapter 3:
+
+// question 1:
+// let age = "19"
+// alert(`My age is ${age}`);
+
+// question 2:
+// let visitors="10 times"
+// alert(`You have visited ${visitors}`);
+
+// question 3:
+// let birthYear = 2005
+// document.write(`My birth year is ${birthYear}`)
+// document.write(`Data type of my declared variable is ${typeof(birthYear)}`)
+
+// question 4:
+// let name= prompt("Enter Your Name:");
+// let product_title = prompt("Enter product title");
+// let quantity = prompt("Enter product quantity");
+// document.write(`${name} ordered ${quantity} ${product_title} on XYZ Clothing Store`)
